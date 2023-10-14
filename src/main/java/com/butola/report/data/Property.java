5b4b0d177a8e3a2af0630a88d6d7b15c7110972e
ybutola <yogibutola@icloud.com>
2023-10-14 06:18:19 -0500

    new data object

---
+++ src/main/java/com/butola/report/data/Property.java
@@ -0,0 +1,23 @@
+package com.butola.report.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Property {
+    private String propertyDate;
+    private String propertyYear;
+    private String propertyPrYear;
+    private String propertyUsefulLive;
+    private String property_0_1;
+    private String property_0_2;
+    private String property_1_1;
+    private String property_1_2;
+    private String property_0_final;
+    private String property_1_final;
+}
