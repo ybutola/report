080a10e8224862f6a8ac6de95c6a4f411b4d2800
ybutola <yogibutola@icloud.com>
2023-10-14 06:16:46 -0500

    new test

---
+++ src/test/java/com/butola/report/listener/ReadAndWriteADocTest.java
@@ -0,0 +1,29 @@
+package com.butola.report.listener;
+
+import com.butola.report.service.worddoc.WriteCashFlow;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.FileOutputStream;
+@ContextConfiguration(classes = {ReadAndWriteADoc.class})
+@ExtendWith(SpringExtension.class)
+class ReadAndWriteADocTest {
+    @Autowired
+    ReadAndWriteADoc readAndWriteADoc;
+
+    @Test
+    void testWriteHeaderParagraph() throws Exception {
+        String wordFilePath = "testoutput22.docx";
+        XWPFDocument document =  readAndWriteADoc.reandNWrite(1);
+
+//
+//        FileOutputStream outputStream = new FileOutputStream(wordFilePath);
+//        document.write(outputStream);
+//        outputStream.close();
+    }
+}
+
